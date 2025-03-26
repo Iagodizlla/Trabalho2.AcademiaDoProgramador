09346f4f8251f720d74547f2226b194f4d2f7d78
Iagodizlla <iagoschlemper@gmail.com>
2025-03-26 17:07:46 -0300

    Implementado codificacao basica

--- Program/Program.cs
+++ Program/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Qual o tamanho da Grid(X e Y)? ");
+            int X = int.Parse(Console.ReadLine()!);
+            int Y = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Qual o posicao inicial do robo?");
+            int Xinicial = int.Parse(Console.ReadLine()!);
+            int Yinicial = int.Parse(Console.ReadLine()!);
+            char ladoinicial = char.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Qual a movimentacao do robo(E, D e M)?");
+            string movi = Console.ReadLine()!;
         }
     }
 }
