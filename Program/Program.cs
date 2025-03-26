b86316c6f44e6076b2652e9b8e999aef33201f39
Iagodizlla <iagoschlemper@gmail.com>
2025-03-26 19:20:40 -0300

    Melhoria na combactacao no calculo da direcao e movimentacao do codigo

--- Program/Program.cs
+++ Program/Program.cs
@@ -10,82 +10,66 @@
             string[] grid = tg.Split(separadores);
             int XM = int.Parse(grid[0]), YM = int.Parse(grid[1]);
 
-            Console.Write("Qual o posicao inicial do robo? ");
-            string pi = Console.ReadLine()!;
-            string[] posicaoI = pi.Split(separadores);
-            int XI = int.Parse(posicaoI[0]), YI = int.Parse(posicaoI[1]);
-            char DA = char.Parse(posicaoI[2]);
+            string robo1 = "", robo2 = "";
 
-            int posicaoXAtual = XI, posicaoYAtual = YI;
-
-            Console.Write("Qual a movimentacao do robo(E, D ou M)? ");
-            string movimentos = Console.ReadLine()!.ToUpper();
-            char[] movi = movimentos.ToCharArray();
-
-
-
-            for (int i = 0;i < movi.Length;i++)
+            for (int j = 0; j < 2; j++)
             {
-                if (movi[i] == 'E')
+                Console.Write($"Qual o posicao inicial do robo {j+1}? ");
+                string pi = Console.ReadLine()!;
+                string[] posicaoI = pi.Split(separadores);
+                int XI = int.Parse(posicaoI[0]), YI = int.Parse(posicaoI[1]);
+                char DA = char.Parse(posicaoI[2].ToUpper());
+                int posicaoXAtual = XI, posicaoYAtual = YI;
+
+                Console.Write("Qual a movimentacao do robo(E, D ou M)? ");
+                string movimentos = Console.ReadLine()!.ToUpper();
+                char[] movi = movimentos.ToCharArray();
+
+                for (int i = 0; i < movi.Length; i++)
                 {
-                    if (DA == 'N')
+                    if (movi[i] == 'E')
                     {
-                        DA = 'O';
+                        switch (DA)
+                        {
+                            case 'N': DA = 'O'; break;
+                            case 'O': DA = 'S'; break;
+                            case 'S': DA = 'L'; break;
+                            case 'L': DA = 'N'; break;
+                        }
                     }
-                    else if (DA == 'O')
+                    else if (movi[i] == 'D')
                     {
-                        DA = 'S';
+                        switch (DA)
+                        {
+                            case 'N': DA = 'L'; break;
+                            case 'O': DA = 'N'; break;
+                            case 'S': DA = 'O'; break;
+                            case 'L': DA = 'S'; break;
+                        }
                     }
-                    else if (DA == 'S')
+                    else if (movi[i] == 'M')
                     {
-                        DA = 'L';
-                    }
-                    else
-                    {
-                        DA = 'N';
+                        switch (DA)
+                        {
+                            case 'N': posicaoYAtual++; break;
+                            case 'O': posicaoXAtual--; break;
+                            case 'S': posicaoYAtual--; break;
+                            case 'L': posicaoXAtual++; break;
+                        }
                     }
                 }
-                else if (movi[i] == 'D')
+                if (j == 0)
                 {
-                    if (DA == 'N')
-                    {
-                        DA = 'L';
-                    }
-                    else if (DA == 'O')
-                    {
-                        DA = 'N';
-                    }
-                    else if (DA == 'S')
-                    {
-                        DA = 'O';
-                    }
-                    else
-                    {
-                        DA = 'S';
-                    }
+                    robo1 = $"{posicaoXAtual} {posicaoYAtual} {DA}";
                 }
-                else if (movi[i] == 'M')
+                else
                 {
-                    if (DA == 'N')
-                    {
-                        posicaoYAtual++;
-                    }
-                    else if (DA == 'O')
-                    {
-                        posicaoXAtual--;
-                    }
-                    else if (DA == 'S')
-                    {
-                        posicaoYAtual--;
-                    }
-                    else
-                    {
-                        posicaoXAtual++;
-                    }
+                    robo2 = $"{posicaoXAtual} {posicaoYAtual} {DA}";
                 }
             }
 
-            Console.WriteLine($"posicao final: {posicaoXAtual} {posicaoYAtual} {DA}");
+            Console.WriteLine($"posicao final robo 1: {robo1}");
+            Console.WriteLine($"posicao final robo 2: {robo2}");
             Console.ReadLine();
         }
     }
