eea74947956e71a65288e2d15dd57b0683d595c4
Iagodizlla <iagoschlemper@gmail.com>
2025-03-31 14:32:06 -0300

    Facilitacao da leitura em Program

--- Program/Program.cs
+++ Program/Program.cs
@@ -9,23 +9,30 @@ namespace Program
         {
             while (true)
             {
+                #region Variaveis
                 bool erro = false;
                 char[] separadores = {' '}, movi;
                 int XM, YM, posicaoXAtual, posicaoYAtual;
                 string[] grid, posicaoI;
                 char m, DA;
                 string robo1 = "", robo2 = "";
-
-                m =Exibicao.ExibirMenu();
-                if(m == '0')
+                #endregion
+                #region Exibir menu inicial
+                m = Exibicao.ExibirMenu();
+                #endregion
+                #region Sair do console
+                if (m == '0')
                 {
                     break;
                 }
+                #endregion
+                #region Fazer simulacao
                 else if (m == '1')
                 {
                     while (true)
                     {
                         grid = PerguntasUsuario.PerguntarTamanhoGrid(separadores);
+                        #region Verifica erro e atrbui valor
                         erro = Verificadores.VerificarArrayGrid(grid, erro);
 
                         if (erro)
@@ -35,11 +42,12 @@ namespace Program
                         XM = int.Parse(grid[0]);
                         YM = int.Parse(grid[1]);
                         break;
+                        #endregion
                     }
-
                     for (int j = 0; j < 2; j++)
                     {
                         posicaoI = PerguntasUsuario.PerguntarPosicaoInicial(separadores, j);
+                        #region Verifica erro e atrbui valor
                         erro = Verificadores.VerificarArrayPosicaoInicial(posicaoI, erro);
 
                         if (erro)
@@ -50,15 +58,15 @@ namespace Program
                         posicaoXAtual = int.Parse(posicaoI[0]);
                         posicaoYAtual = int.Parse(posicaoI[1]);
                         DA = char.Parse(posicaoI[2].ToUpper());
-
                         if (Verificadores.VerificarIfPosicaoInicial(posicaoXAtual, posicaoYAtual, XM, YM, DA))
                         {
                             Console.WriteLine("Posicao inicial invalida");
                             j--;
                             continue;
                         }
-
+                        #endregion
                         movi = PerguntasUsuario.PerguntarMovimentacao();
+                        #region Verifica erro e atrbui valor
                         erro = Verificadores.VerificarArrayMovimentacao(movi, erro);
 
                         if(erro)
@@ -66,6 +74,8 @@ namespace Program
                             j--;
                             continue;
                         }
+                        #endregion
+                        #region Logica do robo
                         for (int i = 0; i < movi.Length; i++)
                         {
                             DA = LogicaRobo.MudarEixoDeDirecao(DA, movi, i);
@@ -75,6 +85,8 @@ namespace Program
                                 posicaoYAtual = LogicaRobo.AlterarDirecaoY(DA, posicaoYAtual);
                             }
                         }
+                        #endregion
+                        #region Atribuir posicao final do robo
                         if (j == 0)
                         {
                             robo1 = Exibicao.AtribuirPosicaoFinalRobo(posicaoXAtual, posicaoYAtual, XM, YM, DA);
@@ -83,17 +95,23 @@ namespace Program
                         {
                             robo2 = Exibicao.AtribuirPosicaoFinalRobo(posicaoXAtual, posicaoYAtual, XM, YM, DA);
                         }
+                        #endregion
                     }
                     Exibicao.ExibirRespostaFinal(robo1, robo2);
                 }
+                #endregion
+                #region Mostrar turorial
                 else if (m == '2')
                 {
                     Exibicao.ExibirTutorial();
                 }
+                #endregion
+                #region Opcao invalida
                 else
                 {
                     Exibicao.ExibirOpcaoMenuInvalido();
                 }
+                #endregion
             }
         }
     }
